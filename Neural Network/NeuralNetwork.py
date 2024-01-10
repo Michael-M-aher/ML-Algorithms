ebbaede0061acec8b9edbe96e0d8091f1116af77
Michael-M-aher <mishoopop6@gmail.com>
2024-01-11 00:06:33 +0200

    Update NeuralNetwork.py

--- Neural Network/NeuralNetwork.py
+++ Neural Network/NeuralNetwork.py
@@ -4,26 +4,48 @@ class Neuron:
     '''
     Neuron class
 
+    Parameters:
+    -----------
+    weights: numpy array
+        The weights of the neuron
+
+    activation: function
+        The activation function of the neuron
+
     Attributes:
-        weights (numpy array): weights of the neuron
-        activation (function): activation function of the neuron
-        bias (float): bias of the neuron
-        error (float): error of the neuron
-        output (float): output of the neuron
+    -----------
+    bias: float
+        The bias of the neuron
+
+    error: float
+        The error of the neuron
+
+    output: float
+        The output of the neuron
 
     Methods:
-        _feed_forward: feed forward the input to the neuron
-        _backpropagation: backpropagate the error to the previous layer
+    --------
+    _feed_forward(inputs)
+        This function feeds forward the input to the neuron.
+
+    _backpropagation(output_error, next_layer, idx)
+        This function backpropagates the error to the previous layer.
     '''
     def __init__(self, weights, activation):
         '''
         Constructor for Neuron class
 
         Parameters:
-            weights (numpy array): weights of the neuron
-            activation (function): activation function of the neuron
+        ----------
+        
+        weights: numpy array
+            The weights of the neuron
+
+        activation: function
+            The activation function of the neuron
 
         Returns:
+        --------
             None
         '''
         self.weights = weights
@@ -37,10 +59,14 @@ class Neuron:
         Feed forward the input to the neuron
 
         Parameters:
-            inputs (numpy array): input to the neuron
+        ----------
+        inputs: numpy array
+            The input to the neuron
 
         Returns:
-            output (float): output of the neuron
+        --------
+        output: float
+            The output of the neuron
         '''
         total = np.dot(self.weights, inputs) + self.bias
         self.output = self.activation(total) if self.activation else total
@@ -51,12 +77,20 @@ class Neuron:
         Backpropagate the error to the previous layer
 
         Parameters:
-            output_error (float): error of the output layer
-            next_layer (Layer): next layer of the neuron
-            idx (int): index of the neuron in the next layer
+        ----------
+        output_error: float
+            The error of the output layer
+
+        next_layer: Layer
+            The next layer of the neuron
+
+        idx: int
+            The index of the neuron in the next layer
 
         Returns:
-            error (float): error of the neuron
+        --------
+        error: float
+            The error of the neuron
         '''
         mul1 = 0
         for neuron in next_layer.neurons:
@@ -69,33 +103,55 @@ class Layer:
     Layer class
 
     Attributes:
-        neurons (list): list of neurons in the layer
-        input_size (int): input size of the layer
-        output_size (int): output size of the layer
-        activation (function): activation function of the layer
-        output (numpy array): output of the layer
+    ----------
+    neurons: list
+        List of neurons in the layer
+
+    input_size: int
+        Input size of the layer
+
+    output_size: int
+        Output size of the layer
+
+    activation: function
+        Activation function of the layer
+
+    output: numpy array
+        Output of the layer
 
     Methods:
-        _feed_forward: feed forward the input to the layer
-        _backpropagation: backpropagate the error to the previous layer
-        _update_weights: update the weights of the layer
-        _update_bias: update the bias of the layer
+    --------
+    _feed_forward(inputs)
+        This function feeds forward the input to the layer.
+
+    _backpropagation(output_error, next_layer)
+        This function backpropagates the error to the previous layer.
+
+    _update_weights(learning_rate, x)
+        This function updates the weights of the layer.
+
+    _update_bias(learning_rate)
+        This function updates the bias of the layer.
     '''
-    def __init__(self, input_size, output_size, activation=None):
+    def __init__(self, output_size, activation=None):
         '''
         Constructor for Layer class
 
         Parameters:
-            input_size (int): input size of the layer
-            output_size (int): output size of the layer
-            activation (function): activation function of the layer
+        ----------
+        output_size: int
+            Output size of the layer
+
+        activation: function
+            Activation function of the layer
 
         Returns:
-            None
+        --------
+        None
         '''
         self.activation = activation if activation else linear
         self.neurons = []
-        self.input_size = input_size
+        self.input_size = 0
         self.output_size = output_size
 
     def _feed_forward(self, inputs):
@@ -103,10 +159,14 @@ class Layer:
         Feed forward the input to the layer
 
         Parameters:
-            inputs (numpy array): input to the layer
+        ----------
+        inputs: numpy array
+            The input to the layer
 
         Returns:
-            outputs (numpy array): outputs of the layer
+        --------
+        outputs: numpy array
+            The outputs of the layer
         '''
         outputs = []
         for neuron in self.neurons:
@@ -118,11 +178,16 @@ class Layer:
         Backpropagate the error to the previous layer
 
         Parameters:
-            output_error (float): error of the output layer
-            next_layer (Layer): next layer of the neuron
+        ----------
+        output_error: float
+            The error of the output layer
+
+        next_layer: Layer
+            The next layer of the neuron
 
         Returns:
-            None
+        --------
+        None
         '''
         for i in range(len(self.neurons)):
             error = self.neurons[i]._backpropagation(output_error, next_layer, i)
@@ -133,11 +198,16 @@ class Layer:
         Update the weights of the layer
 
         Parameters:
-            learning_rate (float): learning rate of the network
-            x (numpy array): input to the layer
+        ----------
+        learning_rate: float
+            Learning rate of the network
+
+        x: numpy array
+            Input to the layer
 
         Returns:
-            None
+        --------
+        None
         '''
         for neuron in self.neurons:
             neuron.weights -= learning_rate * neuron.error * x
@@ -147,40 +217,72 @@ class Layer:
         Update the bias of the layer
 
         Parameters:
-            learning_rate (float): learning rate of the network
+        ----------
+        learning_rate: float
+            Learning rate of the network
 
         Returns:
-            None
+        --------
+        None
         '''
         for neuron in self.neurons:
             neuron.bias -= learning_rate * neuron.error[0]
     
+    def __str__(self):
+        return f'Layer ({self.input_size}, {self.output_size}) with %d neurons' % len(self.neurons)
+    
     def __repr__(self):
-        return 'Layer with %d neurons' % len(self.neurons)
+        return self.__str__()
     
 class InputLayer(Layer):
     '''
     Input Layer class
 
+    Parameters:
+    ----------
+    input_size: int
+        Input size of the layer
+
     Attributes:
-        neurons (list): list of neurons in the layer
-        input_size (int): input size of the layer
-        output_size (int): output size of the layer
-        activation (function): activation function of the layer
-        output (numpy array): output of the layer
+    ----------
+    neurons: list
+        List of neurons in the layer
+
+    output_size: int
+        Output size of the layer
+
+    activation: function
+        Activation function of the layer
+
+    output: numpy array
+        Output of the layer
 
     Methods:
-        _feed_forward: feed forward the input to the layer
+    --------
+    _feed_forward(inputs)
+        This function feeds forward the input to the layer.
+
+    _backpropagation(output_error, next_layer)
+        This function backpropagates the error to the previous layer.
+
+    _update_weights(learning_rate, x)
+        This function updates the weights of the layer.
+
+    _update_bias(learning_rate)
+        This function updates the bias of the layer.
     '''
     def __init__(self, input_size):
         '''
         Constructor for Input Layer class
 
         Parameters:
-            input_size (int): input size of the layer
+        ----------
+        input_size: int
+            Input size of the layer
 
         Returns:
-            None
+        --------
+        None
         '''
         super().__init__(input_size, input_size)
     
@@ -189,46 +291,73 @@ class InputLayer(Layer):
         Feed forward the input to the layer
 
         Parameters:
-            inputs (numpy array): input to the layer
+        ----------
+        inputs: numpy array
+            The input to the layer
 
         Returns:
-            inputs (numpy array): outputs of the layer
+        --------
+        inputs: numpy array
+            The input to the layer
         '''
         return inputs
     
+    def __str__(self):
+        return f'Input Layer ({self.input_size}, {self.output_size}) with %d neurons' % len(self.neurons)
+    
     def __repr__(self):
-        return 'Input Layer with %d neurons' % len(self.neurons)
+        return self.__str__()
     
     
 class NeuralNetwork:
     '''
     Neural Network class
 
+    Parameters:
+    ----------
+    layers: list
+        List of layers in the network
+
     Attributes:
-        layers (list): list of layers in the network
-        input_layer (InputLayer): input layer of the network
-        output_layer (Layer): output layer of the network
+    ----------
+    layers: list
+        List of layers in the network
+
+    input_layer: InputLayer
+        Input layer of the network
+
+    output_layer: Layer
+        Output layer of the network
 
     Methods:
-        _init_layers: initialize the layers of the network
-        _forward: feed forward the input to the network
-        _backward: backpropagate the error to the previous layer
-        _mse: calculate the mean squared error
-        _mse_derivative: calculate the derivative of the mean squared error
-        fit: fit the network to the data
-        predict: predict the output of the network
-        calculate_accuracy: calculate the accuracy of the network
-        evaluate: evaluate the network on the data
+    --------
+    fit(input, target, epochs, learning_rate)
+        fit the network to the data
+
+    predict(input)
+        predict the output of the network
+
+    calculate_accuracy(y_pred, y_test)
+        calculate the accuracy of the network
+
+    evaluate(input, target)
+        evaluate the network on the data
+
+    summary()
+        print the summary of the network
     '''
     def __init__(self, layers):
         '''
         Constructor for Neural Network class
 
         Parameters:
-            layers (list): list of layers in the network
+        ----------
+        layers: list
+            List of layers in the network
 
         Returns:
-            None
+        --------
+        None
         '''
         layers = self._init_layers(layers)
         self.layers = layers[1:-1]
@@ -240,16 +369,25 @@ class NeuralNetwork:
         Initialize the layers of the network
 
         Parameters:
-            layers (list): list of layers in the network
+        ----------
+        layers: list
+            List of layers in the network
 
         Returns:
-            layers (list): list of layers in the network
+        --------
+        layers: list
         '''
+        last_output_size = layers[0].output_size
+        for layer in layers:
+            layer.input_size = last_output_size
+            last_output_size = layer.output_size
+
         total_neurons = 0
         for layer in layers:
             total_neurons += layer.input_size * layer.output_size
 
         for i in range(len(layers)-1):
+            
             for _ in range(layers[i].output_size):
                 rng = np.random.default_rng()
                 weights = rng.uniform(-1/total_neurons, 1/total_neurons ,layers[i].input_size)
@@ -265,10 +403,14 @@ class NeuralNetwork:
         Feed forward the input to the network
 
         Parameters:
-            input (numpy array): input to the network
+        ----------
+        input: numpy array
+            The input to the network
 
         Returns:
-            output (numpy array): output of the network
+        --------
+        output: numpy array
+            The output of the network
         '''
         output = input
         for layer in self.layers:
@@ -283,10 +425,13 @@ class NeuralNetwork:
         Backpropagate the error to the previous layer
 
         Parameters:
-            output_error (float): error of the output layer
+        ----------
+        output_error: float
+            The error of the output layer
 
         Returns:
-            None
+        --------
+        None
         '''
         next_layer = self.output_layer
         for layer in reversed(self.layers):
@@ -298,11 +443,17 @@ class NeuralNetwork:
         Calculate the mean squared error
 
         Parameters:
-            y_true (numpy array): true output
-            y_pred (numpy array): predicted output
+        ----------
+        y_true : numpy array
+            true output
+
+        y_pred : numpy array
+            predicted output
 
         Returns:
-            error (float): mean squared error
+        --------
+        error : float
+            mean squared error
         '''
         return np.square(np.subtract(y_true, y_pred)).mean()
 
@@ -311,11 +462,17 @@ class NeuralNetwork:
         Calculate the derivative of the mean squared error
 
         Parameters:
-            y_true (numpy array): true output
-            y_pred (numpy array): predicted output
+        ----------
+        y_true : numpy array
+            true output
+
+        y_pred : numpy array
+            predicted output
 
         Returns:
-            error (float): derivative of the mean squared error
+        --------
+        error : float
+            derivative of the mean squared error
         '''
         return (y_pred - y_true) * y_pred * (1 - y_pred)
     
@@ -324,13 +481,22 @@ class NeuralNetwork:
         Fit the network to the data
 
         Parameters:
-            input (numpy array): input to the network
-            target (numpy array): target output
-            epochs (int): number of epochs
-            learning_rate (float): learning rate of the network
+        ----------
+        input : numpy array
+            The input to the network
+
+        target : numpy array
+            The target output
+
+        epochs : int
+            Number of epochs to train the network
+
+        learning_rate : float
+            Learning rate of the network
 
         Returns:
-            None
+        --------
+        None
         '''
         for epoch in range(epochs):
             error = 0
@@ -354,10 +520,13 @@ class NeuralNetwork:
         Predict the output of the network
 
         Parameters:
-            input (numpy array): input to the network
+        ----------
+        input : numpy array
+            The input to the network
 
         Returns:
-            output (numpy array): output of the network
+        --------
+        output : numpy array
         '''
         output = []
         for x in input:
@@ -369,11 +538,17 @@ class NeuralNetwork:
         Calculate the accuracy of the network
 
         Parameters:
-            y_pred (numpy array): predicted output
-            y_test (numpy array): true output
+        ----------
+        y_pred : numpy array
+            The predicted output
+
+        y_test : numpy array
+            The target output
 
         Returns:
-            error (float): accuracy of the network
+        --------
+        error : float
+            The mean squared error
         '''
         return self._mse(y_test, y_pred)
     
@@ -382,21 +557,47 @@ class NeuralNetwork:
         Evaluate the network on the data
 
         Parameters:
-            input (numpy array): input to the network
-            target (numpy array): target output
+        ----------
+        input : numpy array
+            The input to the network
+            
+        target : numpy array
+            The target output
 
         Returns:
-            None
+        --------
+        None
         '''
         output = self.predict(input)
         error = self.calculate_accuracy(output, target)
         print('Error: %f' % error)
-
-    def __repr__(self):
-        return 'Neural Network with %d layers' % len(self.layers)
     
+    def summary(self):
+        '''
+        Print the summary of the network
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        --------
+        None
+        '''
+        print(self)
+        print('[')
+        print(self.input_layer)
+        for layer in self.layers:
+            print(layer)
+        print(self.output_layer)
+        print(']')
+
     def __str__(self):
-        return 'Neural Network with %d layers' % len(self.layers)
+        return 'Neural Network with %d layers' % (len(self.layers)+2)
+    
+    def __repr__(self):
+        return self.__str__()
+    
 
 
 def sigmoid(x):
@@ -404,10 +605,14 @@ def sigmoid(x):
     Sigmoid activation function
 
     Parameters:
-        x (float): input to the activation function
+    ----------
+    x : float
+        input to the activation function
 
     Returns:
-        x (float): output of the activation function
+    --------
+    x : float
+        output of the activation function
     '''
     return 1/(1 + np.exp(-x))
 
@@ -416,10 +621,14 @@ def relu(x):
     ReLU activation function
 
     Parameters:
-        x (float): input to the activation function
+    ----------
+    x : float
+        input to the activation function
 
     Returns:
-        x (float): output of the activation function
+    --------
+    x : float
+        output of the activation function
     '''
     return max(0 , x)
 
@@ -428,9 +637,13 @@ def linear(x):
     Linear activation function
 
     Parameters:
-        x (float): input to the activation function
+    ----------
+    x : float
+        input to the activation function
 
     Returns:
-        x (float): output of the activation function
+    --------
+    x : float
+        output of the activation function
     '''
     return x
